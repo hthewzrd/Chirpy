08496439b522306feb703e5a4fbe79a3f0dc7799
hthewzrd <dhund.data@gmail.com>
2026-04-29 11:28:12 +0100

    http handles updated, moved to decoupled architecture and metrics pages setup

--- main.go
+++ main.go
@@ -1,29 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
+	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", handlerReadiness)
+	fsHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fsHandler))
 
-	fs := http.FileServer(http.Dir("."))
 
-	mux.Handle("/app/", http.StripPrefix("/app", fs))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from . on port: %s\n", port)
-	
+	log.Printf("Serving Chirpy on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	htmlTemplate := `
+	<html>
+	  <body>
+	    <h1>Welcome, Chirpy Admin</h1>
+	    <p>Chirpy has been visited %d times!</p>
+	  </body>
+	</html>`
+
+	fmt.Fprintf(w, htmlTemplate, cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
