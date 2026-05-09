450e45b7cf94877d91a9322e5f4a7aa88a3d65ae
hthewzrd <dhund.data@gmail.com>
2026-05-09 13:15:23 +0100

    password auth, jwt token functionality added

--- go.mod
+++ go.mod
@@ -1,9 +1,16 @@
 module chirpy
 
-go 1.25.5
+go 1.26.0
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.12.3 // indirect
+	github.com/alexedwards/argon2id v1.0.0
+	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
+
+require (
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"database/sql"
 	"net/http"
 
 	"github.com/google/uuid"
 )
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	chirpIDString := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
+		return
+	}
+
+	dbChirp, err := cfg.db.GetChirp(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get chirp", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		UserID:    dbChirp.UserID,
+		Body:      dbChirp.Body,
+	})
+}
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := cfg.db.GetChirps(r.Context())
@@ -26,31 +48,4 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
-}
-
-func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
-	chirpIDString := r.PathValue("chirpID")
-	chirpID, err := uuid.Parse(chirpIDString)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
-		return
-	}
-
-	dbChirp, err := cfg.db.GetChirp(r.Context(), chirpID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			respondWithError(w, http.StatusNotFound, "Chirp not found", err)
-			return
-		}
-		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirp", err)
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, Chirp{
-		ID:        dbChirp.ID,
-		CreatedAt: dbChirp.CreatedAt,
-		UpdatedAt: dbChirp.UpdatedAt,
-		UserID:    dbChirp.UserID,
-		Body:      dbChirp.Body,
-	})
 }
